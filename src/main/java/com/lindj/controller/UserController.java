d2dead6908000418a3af2b5b08b9d059d30e73ca
lindj <2118089226@qq.com>
2018-02-11 14:22:19 +0800

    注册中心地址

--- src/main/java/com/lindj/controller/UserController.java
+++ src/main/java/com/lindj/controller/UserController.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by lindongjie on 17/12/1.
  */
 
+@RequestMapping("/user")
 @RestController
 public class UserController {
 
     @RequestMapping(value = "/user/say",method= RequestMethod.GET)
     public String sayHello(){
         return "hello world !";
+    }
+
+    @RequestMapping(value = "/send",method= RequestMethod.GET)
+    public String sendMessage(){
+
+        return null;
     }
 }
